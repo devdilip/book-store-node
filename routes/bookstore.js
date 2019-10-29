ce84f93dd9afcc3ae681a2db60332dd3857620d8
Dilip <dilipkr7587@gmail.com>
2019-10-29 17:21:27 +0530

    Added purchase book api

--- routes/bookstore.js
+++ routes/bookstore.js
@@ -34,4 +34,31 @@ route.get("/store/list-of-book", validator.validateToken, (req, res) => {
     .catch(error => res.status(400).send(error));
 });
 
+route.get("/store/purchase/books", validator.validateToken, (req, res) => {
+  let { book_id, email } = req.query;
+  Book.find({ _id: book_id })
+    .then(data => {
+      if (data.length === 0) {
+        return res.status(200).json({
+          status: false,
+          message: "book is not available"
+        });
+      } else {
+        let newPurchase = new PurchaseBook();
+        newPurchase.userEmail = email;
+        newPurchase.book_id = data[0]._id;
+        newPurchase
+          .save()
+          .then(status => {
+            return res.status(200).json({
+              status: true,
+              message: "successful! Book purchase"
+            });
+          })
+          .catch(error => res.status(400).send(error));
+      }
+    })
+    .catch(error => res.status(400).send(error));
+});
+
 module.exports = route;
