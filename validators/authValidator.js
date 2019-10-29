74541aadb803aac060cdb922c5faa5db32e2b232
Dilip <dilipkr7587@gmail.com>
2019-10-29 16:57:50 +0530

    Added jwt token validator

---
+++ validators/authValidator.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+let authConfig = require("./config/authConfig");
+
+let validateToken = (request, response, next) => {
+  let token = request.headers["authorization"];
+  if (token && token.startsWith("Bearer ")) {
+    token = token.slice(7, token.length);
+  }
+
+  if (token) {
+    jwt.verify(token, authConfig.secret_key, (err, decode) => {
+      if (err) {
+        response.json({
+          message: "token not valid",
+          status: false
+        });
+      } else {
+        request.decode = decode;
+        next();
+      }
+    });
+  } else {
+    response.json({
+      message: "token is missing",
+      status: false
+    });
+  }
+};
+
+module.exports = {
+  validateToken: validateToken
+};
