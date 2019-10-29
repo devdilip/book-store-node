e8e028520ebb8e33522a0649af5407d5b727c355
Dilip <dilipkr7587@gmail.com>
2019-10-29 17:20:46 +0530

    Added book list api

--- routes/bookstore.js
+++ routes/bookstore.js
@@ -4,6 +4,7 @@ const route = express.Router();
 let validator = require("../validators/auth");
 
 const Book = require("../schema/book");
+const PurchaseBook = require("../schema/purchase");
 
 route.post("/store/add-book", validator.validateToken, (req, res) => {
   let { title, price, description } = req.body;
@@ -17,6 +18,17 @@ route.post("/store/add-book", validator.validateToken, (req, res) => {
       return res.status(201).json({
         status: true,
         message: "book store successfully"
+      });
+    })
+    .catch(error => res.status(400).send(error));
+});
+
+route.get("/store/list-of-book", validator.validateToken, (req, res) => {
+  Book.find({})
+    .then(data => {
+      return res.status(200).json({
+        list: data,
+        status: true
       });
     })
     .catch(error => res.status(400).send(error));
